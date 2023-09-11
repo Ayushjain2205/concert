8eac80433ff550d207d207410aa23c738d92fda3
Ayush Jain <ayushjain2205@gmail.com>
2023-09-12 01:31:14 +0530

    Configure thirweb sdk

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,17 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/UI/Layout";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ThirdwebProvider
+      activeChain="ethereum"
+      clientId={process.env.NEXT_PUBLIC_CLIENT_ID}
+    >
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ThirdwebProvider>
   );
 }
