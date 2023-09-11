f4382e949bc5b015becc889154969f587c561da2
Ayush Jain <ayushjain2205@gmail.com>
2023-09-11 17:10:01 +0530

    Add layout component

--- src/components/UI/Layout.js
+++ src/components/UI/Layout.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Navbar from "./Navbar";
 
 const Layout = ({ children }) => {
-  return <div>{children}</div>;
+  return (
+    <div>
+      <Navbar />
+      <div className="mx-[48px] my-[32px]">{children}</div>
+    </div>
+  );
 };
 
 export default Layout;
