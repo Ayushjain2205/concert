a3474b8df17512b1ee7f8859c3144ce925d1a842
Ayush Jain <ayushjain2205@gmail.com>
2023-09-12 01:35:30 +0530

    Add filecoin testnet and mainnet

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,13 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/UI/Layout";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { FilecoinCalibrationTestnet, Filecoin } from "@thirdweb-dev/chains";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider
-      activeChain="ethereum"
+      activeChain={FilecoinCalibrationTestnet}
+      supportedChains={[FilecoinCalibrationTestnet, Filecoin]}
       clientId={process.env.NEXT_PUBLIC_CLIENT_ID}
     >
       <Layout>
