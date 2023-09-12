568e7ff96fb811a33c12f6655e3e9bcf04f02527
Ayush Jain <ayushjain2205@gmail.com>
2023-09-13 00:27:55 +0530

    Add font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,15 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html data-theme="light" lang="en">
-      <Head />
+      <Head>
+        <title>Data Royale</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        ></link>
+      </Head>
       <body data-theme="royale">
         <Main />
         <NextScript />
