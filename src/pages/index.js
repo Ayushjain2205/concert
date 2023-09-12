d675fcf2886975e5df9d00207fbceca968d1e11c
Ayush Jain <ayushjain2205@gmail.com>
2023-09-13 02:01:24 +0530

    Add landing page with create and explore cards

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,13 @@
 import React from "react";
+import LandingCard from "../components/Cards/LandingCard";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="flex flex-row gap-[100px] justify-center align-center mt-[150px]">
+      <LandingCard type="create" />
+      <LandingCard type="explore" />
+    </div>
+  );
 };
 
 export default Home;
