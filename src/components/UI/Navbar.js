3a12724ba9bf96ccbdde6f9389be7a055a4e3035
Ayush Jain <ayushjain2205@gmail.com>
2023-09-13 00:22:01 +0530

    Customize connect wallet button

--- src/components/UI/Navbar.js
+++ src/components/UI/Navbar.js
@@ -5,13 +5,13 @@ const Navbar = () => {
   return (
     <div className="navbar bg-base-100">
       <div className="flex-1">
-        <a className="btn btn-ghost normal-case text-xl text-primary">
+        <a className="btn btn-ghost normal-case text-xl text-primary hover:bg-white">
           <img src="/data-royale-logo.svg" className="h-[40px]" alt="" />
           Data Royale
         </a>
       </div>
-      <div className="flex-none">
-        <ConnectWallet />
+      <div className="flex-none mx-[20px]">
+        <ConnectWallet className="connect-button" />
       </div>
     </div>
   );
