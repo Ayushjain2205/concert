5879ea6540e2c75f6198d9738c564914dea824fc
Ayush Jain <ayushjain2205@gmail.com>
2023-09-13 17:36:03 +0530

    Update search bar

--- src/components/Functional/SearchBar.js
+++ src/components/Functional/SearchBar.js
@@ -6,9 +6,9 @@ const SearchBar = () => {
       <input
         type="text"
         placeholder="Search datasets"
-        className="input input-bordered border-2 input-primary w-full focus:outline-none pl-4 pr-10 rounded-[20px]" // adjusted padding to ensure text doesn't overlap with the icon
+        className="input input-bordered border-2 border-[#B8B8FF] input-primary w-full focus:outline-none pl-4 pr-10 rounded-[20px]"
       />
-      <i className="fa-solid fa-magnifying-glass absolute top-1/2 transform -translate-y-1/2 right-5 text-primary"></i>
+      <i className="fa-solid fa-magnifying-glass absolute top-1/2 transform -translate-y-1/2 right-5 text-[#B8B8FF]"></i>
     </div>
   );
 };
