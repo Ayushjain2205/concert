a09fbae52909a07d5153ddb5ceb5d409bed04a81
Ayush Jain <ayushjain2205@gmail.com>
2023-09-13 15:06:44 +0530

    Side bar basic scaffold

--- src/pages/explore.js
+++ src/pages/explore.js
@@ -1,10 +1,14 @@
 import React from "react";
 import SearchBar from "../components/Functional/SearchBar";
+import SideBar from "../components/Functional/SideBar";
 
 const Explore = () => {
   return (
     <div className="flex flex-col items-center gap-[40px]">
       <SearchBar />
+      <div className="flex flex-row">
+        <SideBar />
+      </div>
     </div>
   );
 };
