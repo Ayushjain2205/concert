347c37f93cf4013e565b592976ce401041b4a1ce
Ayush Jain <ayushjain2205@gmail.com>
2023-09-14 12:43:54 +0530

    Add file previewer

--- src/components/Buttons/ViewFile.js
+++ src/components/Buttons/ViewFile.js
@@ -1,6 +1,10 @@
 import React from "react";
+import FileViewer from "react-file-viewer";
 
 const ViewFile = () => {
+  const file =
+    "https://w7.pngwing.com/pngs/895/199/png-transparent-spider-man-heroes-download-with-transparent-background-free-thumbnail.png";
+  const type = "png";
   return (
     <div>
       <button
@@ -11,8 +15,12 @@ const ViewFile = () => {
       </button>
       <dialog id="my_modal_2" className="modal">
         <div className="modal-box">
-          <h3 className="font-bold text-lg">Hello!</h3>
-          <p className="py-4">Press ESC key or click outside to close</p>
+          <h3 className="font-bold text-lg">File</h3>
+          <FileViewer
+            fileType={type}
+            filePath={file}
+            onError={(error) => console.log(error)}
+          />
         </div>
         <form method="dialog" className="modal-backdrop">
           <button>close</button>
