9a9054e1e1507fdea0230518d7a8105263a9d3ea
Ayush Jain <ayushjain2205@gmail.com>
2023-09-15 02:41:26 +0530

    Add Downloads Chart

--- src/components/Sections/Stats.js
+++ src/components/Sections/Stats.js
@@ -3,7 +3,8 @@ import Chart1 from "../Charts/Chart1";
 import Chart2 from "../Charts/Chart2";
 import Chart3 from "../Charts/Chart3";
 import Chart4 from "../Charts/Chart4";
-import Chart5 from "../Charts/Charts5";
+import Chart5 from "../Charts/Chart5";
+import Chart6 from "../Charts/Chart6";
 
 const Stats = () => {
   return (
@@ -23,7 +24,9 @@ const Stats = () => {
       <div className="rounded-xl bg-white p-4 ring ring-indigo-50 w-[334px] h-[225px]">
         <Chart5 />
       </div>
-      <div className="rounded-xl bg-white p-4 ring ring-indigo-50 w-[334px] h-[225px]"></div>
+      <div className="rounded-xl bg-white p-4 ring ring-indigo-50 w-[334px] h-[225px]">
+        <Chart6 />
+      </div>
     </div>
   );
 };
