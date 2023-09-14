84fbda85b331aa4ef7968a42c13b3ba11c6de1d5
Ayush Jain <ayushjain2205@gmail.com>
2023-09-15 02:08:54 +0530

    Add contributions chart

--- src/components/Sections/Stats.js
+++ src/components/Sections/Stats.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Chart1 from "../Charts/Chart1";
 import Chart2 from "../Charts/Chart2";
+import Chart3 from "../Charts/Chart3";
 
 const Stats = () => {
   return (
@@ -11,7 +12,9 @@ const Stats = () => {
       <div className="rounded-xl bg-white p-4 ring ring-indigo-50 w-[334px] h-[225px]">
         <Chart2 />
       </div>
-      <div className="rounded-xl bg-white p-4 ring ring-indigo-50 w-[334px] h-[225px]"></div>
+      <div className="rounded-xl bg-white p-4 ring ring-indigo-50 w-[334px] h-[225px]">
+        <Chart3 />
+      </div>
       <div className="rounded-xl bg-white p-4 ring ring-indigo-50 w-[334px] h-[225px]"></div>
       <div className="rounded-xl bg-white p-4 ring ring-indigo-50 w-[334px] h-[225px]"></div>
       <div className="rounded-xl bg-white p-4 ring ring-indigo-50 w-[334px] h-[225px]"></div>
