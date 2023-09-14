e04404122a1f08f0a21220f9709fe1d58bb5e884
Ayush Jain <ayushjain2205@gmail.com>
2023-09-14 23:50:27 +0530

    Fix header title warning

--- src/components/UI/Layout.js
+++ src/components/UI/Layout.js
@@ -1,9 +1,13 @@
 import React from "react";
 import Navbar from "./Navbar";
+import Head from "next/head";
 
 const Layout = ({ children }) => {
   return (
     <div>
+      <Head>
+        <title>Data Royale</title>
+      </Head>
       <Navbar />
       <div className="mx-[48px] my-[32px]">{children}</div>
     </div>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html data-theme="light" lang="en">
       <Head>
-        <title>Data Royale</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
