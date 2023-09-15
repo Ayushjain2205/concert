aaf0d1136a898fe5315a34cfa80e85985b40bc4a
Ayush Jain <ayushjain2205@gmail.com>
2023-09-16 00:56:31 +0530

    Refactor AI card and add PDFAI

--- src/components/Cards/AICard.js
+++ src/components/Cards/AICard.js
@@ -1,17 +1,14 @@
 import React from "react";
 
-const AICard = () => {
+const AICard = ({ title, image, description, id }) => {
   return (
     <div className="flex flex-row rounded-xl bg-white p-4 ring ring-indigo-50 h-[200px] w-[500px] cursor-pointer hover:bg-[#B8B8FF30]">
       <div className="flex flex-col gap-[30px]">
         <div className="flex flex-row items-center gap-[20px]">
-          <img src="/ai-icons/file-chat.svg" className="h-[75px]" alt="" />
-          <p className="text-[36px]">PDF summary</p>
+          <img src={image} className="h-[75px]" alt={title} />
+          <p className="text-[36px]">{title}</p>
         </div>
-        <p className="opacity-50">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur
-          eum repudiandae voluptatibus quidem numquam,
-        </p>
+        <p className="opacity-50">{description}</p>
       </div>
     </div>
   );
--- src/components/Sections/AI/PdfAI.js
+++ src/components/Sections/AI/PdfAI.js
@@ -2,11 +2,39 @@ import React from "react";
 import AICard from "../../Cards/AICard";
 
 const PdfAI = () => {
+  const cardData = [
+    {
+      title: "File summary",
+      image: "/ai-icons/file-summary.svg",
+      description: "Get the summary of a PDF file.",
+      id: "file-summary",
+    },
+    {
+      title: "Chat with file",
+      image: "/ai-icons/file-chat.svg",
+      description:
+        "Chat with a PDF file, ask questions get contextual relevant answers.",
+      id: "file-chat",
+    },
+    {
+      title: "Generate content",
+      image: "/ai-icons/generate-content.svg",
+      description: "Generate content based on a file.",
+      id: "generate-content",
+    },
+  ];
+
   return (
     <div className="grid grid-cols-2 gap-[40px] my-[20px]">
-      <AICard />
-      <AICard />
-      <AICard />
+      {cardData.map((card) => (
+        <AICard
+          key={card.id}
+          title={card.title}
+          image={card.image}
+          description={card.description}
+          id={card.id}
+        />
+      ))}
     </div>
   );
 };
