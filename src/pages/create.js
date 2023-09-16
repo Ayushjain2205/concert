1412a2e31a2d69c1060525c16122c128a4cf20e4
Ayush Jain <ayushjain2205@gmail.com>
2023-09-16 21:16:00 +0530

    Basic stepper logic

--- src/pages/create.js
+++ src/pages/create.js
@@ -1,7 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Create = () => {
-  return <div>create</div>;
+  const [currentStep, setCurrentStep] = useState(0);
+  const steps = [
+    { label: "Register", content: "Content for Register" },
+    { label: "Choose plan", content: "Content for Choose plan" },
+    { label: "Purchase", content: "Content for Purchase" },
+    { label: "Receive Product", content: "Content for Receive Product" },
+  ];
+
+  const goNext = () => {
+    if (currentStep < steps.length - 1) {
+      setCurrentStep(currentStep + 1);
+    }
+  };
+
+  const goPrev = () => {
+    if (currentStep > 0) {
+      setCurrentStep(currentStep - 1);
+    }
+  };
+
+  const onFinish = () => {
+    console.log("Finish Clicked"); // Replace this with your onFinish logic
+  };
+
+  return (
+    <div className="flex flex-col gap-[20px]">
+      <ul className="steps">
+        {steps.map((step, index) => (
+          <li
+            key={index}
+            className={`step ${currentStep >= index ? "step-primary" : ""}`}
+          >
+            {step.label}
+          </li>
+        ))}
+      </ul>
+      <div className="step-content rounded-xl bg-white p-4 ring ring-indigo-50">
+        {steps[currentStep].content}
+      </div>
+      <div className="navigation-buttons">
+        {currentStep > 0 && <button onClick={goPrev}>Previous</button>}
+        {currentStep < steps.length - 1 && (
+          <button onClick={goNext}>Next</button>
+        )}
+        {currentStep === steps.length - 1 && (
+          <button onClick={onFinish}>Finish</button>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Create;
