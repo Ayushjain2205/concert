9e7a569bac29f18aff9c128fc8c08f57c3f292c7
Ayush Jain <ayushjain2205@gmail.com>
2023-09-17 01:22:01 +0530

    Add rules switches

--- src/components/Steps/Step2.js
+++ src/components/Steps/Step2.js
@@ -1,4 +1,5 @@
 import React from "react";
+import * as Switch from "@radix-ui/react-switch";
 
 const Step2 = () => {
   return (
@@ -32,7 +33,35 @@ const Step2 = () => {
           <input type="checkbox" className="checkbox checkbox-primary" />
         </label>
       </div>
-      <div className="flex flex-col w-1/3 "></div>
+      <div className="flex flex-col gap-[10px] w-1/3 ">
+        <label className="label">
+          <span className="label-text text-[16px]">Actions:</span>
+        </label>
+        <div className="flex flex-row justify-between pl-[3px]">
+          <label className="text-[20px]">Allow Download</label>
+          <Switch.Root className="w-[42px] h-[25px] rounded-full relative bg-[#9381FF30] data-[state=checked]:bg-[#9381FF] outline-none cursor-default">
+            <Switch.Thumb className="block w-[21px] h-[21px] bg-white rounded-full transition-transform duration-100 translate-x-0.5 will-change-transform data-[state=checked]:translate-x-[19px]" />
+          </Switch.Root>
+        </div>
+        <div className="flex flex-row justify-between pl-[3px]">
+          <label className="text-[20px]">Allow Fork</label>
+          <Switch.Root className="w-[42px] h-[25px] rounded-full relative bg-[#9381FF30] data-[state=checked]:bg-[#9381FF] outline-none cursor-default">
+            <Switch.Thumb className="block w-[21px] h-[21px] bg-white rounded-full transition-transform duration-100 translate-x-0.5 will-change-transform data-[state=checked]:translate-x-[19px]" />
+          </Switch.Root>
+        </div>
+        <div className="flex flex-row justify-between pl-[3px]">
+          <label className="text-[20px]">Allow Contributions</label>
+          <Switch.Root className="w-[42px] h-[25px] rounded-full relative bg-[#9381FF30] data-[state=checked]:bg-[#9381FF] outline-none cursor-default">
+            <Switch.Thumb className="block w-[21px] h-[21px] bg-white rounded-full transition-transform duration-100 translate-x-0.5 will-change-transform data-[state=checked]:translate-x-[19px]" />
+          </Switch.Root>
+        </div>
+        <div className="flex flex-row justify-between pl-[3px]">
+          <label className="text-[20px]">Allow API Access</label>
+          <Switch.Root className="w-[42px] h-[25px] rounded-full relative bg-[#9381FF30] data-[state=checked]:bg-[#9381FF] outline-none cursor-default">
+            <Switch.Thumb className="block w-[21px] h-[21px] bg-white rounded-full transition-transform duration-100 translate-x-0.5 will-change-transform data-[state=checked]:translate-x-[19px]" />
+          </Switch.Root>
+        </div>
+      </div>
     </div>
   );
 };
