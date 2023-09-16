0bebeb72f7446a0c30d2aa27485795664e0334f6
Ayush Jain <ayushjain2205@gmail.com>
2023-09-16 15:34:59 +0530

    Add react-code-display

--- src/components/Sections/Code.js
+++ src/components/Sections/Code.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { CopyBlock, dracula } from "react-code-blocks";
 
 const Code = () => {
-  return <div>Code</div>;
+  const code = `
+  print("Hello World")
+  print("Hello World")
+  print("Hello World")
+  `;
+  return (
+    <div className="flex flex-row">
+      <div className="flex flex-col w-2/5"></div>
+      <div className="flex flex-col w-3/5">
+        <CopyBlock text={code} language="python" theme={dracula} />
+      </div>
+    </div>
+  );
 };
 
 export default Code;
