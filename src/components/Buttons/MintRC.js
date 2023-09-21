f871cd79d29d348b61a50f690bf27ae91f32188f
Ayush Jain <ayushjain2205@gmail.com>
2023-09-21 22:11:20 +0530

    update buy button

--- src/components/Buttons/MintRC.js
+++ src/components/Buttons/MintRC.js
@@ -1,22 +1,22 @@
 import React, { useState } from "react";
-import { useSigner } from "@thirdweb-dev/react";
+import { useSigner, useAddress } from "@thirdweb-dev/react";
 import abi from "../../abi/RoyalCoin";
 import { ethers } from "ethers";
 import { successToast, errorToast } from "../../helpers/showToast";
 
 const contractAddress = "0xDb499857812569403F0aA1036d453d30945C8751";
-const walletAddress = "0xCafa93E9985793E2475bD58B9215c21Dbd421fD0"; // This seems unused in the original code, but I've kept it here just in case.
 
 const MintRC = () => {
-  const [inputValue, setInputValue] = useState(100);
+  const [inputValue, setInputValue] = useState(1000);
   const signer = useSigner();
+  const address = useAddress();
 
   // Initialize the contract instance with the signer
   const contract = new ethers.Contract(contractAddress, abi, signer);
 
   // This function will be executed when the minting button is pressed
   const mintTokens = async () => {
-    let to = walletAddress; // Assuming this is the recipient address for minting
+    let to = address;
     const amount = ethers.utils.parseUnits(inputValue.toString(), 0);
 
     try {
@@ -33,7 +33,7 @@ const MintRC = () => {
       onClick={mintTokens} // Linking the mintTokens function to the button
       className="btn btn-outline btn-primary rounded-xl w-full outline-none"
     >
-      Buy
+      Buy 1000
       <img src="/icons/royale-coin.svg" className="h-[32px]" alt="" />
     </button>
   );
