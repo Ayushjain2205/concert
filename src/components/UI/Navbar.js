5f577535dcc640fcf10477c386a270596391f04c
Ayush Jain <ayushjain2205@gmail.com>
2023-09-26 13:23:18 +0530

    init landing page

--- src/components/UI/Navbar.js
+++ src/components/UI/Navbar.js
@@ -4,11 +4,13 @@ import Link from "next/link";
 import { ethers } from "ethers";
 import MintRC from "../Buttons/MintRC";
 import * as Popover from "@radix-ui/react-popover";
+import { useRouter } from "next/router";
 
 const Navbar = () => {
   const address = useAddress();
   const tokenAddress = "0xDb499857812569403F0aA1036d453d30945C8751";
   const { data, isLoading } = useBalance(tokenAddress);
+  const router = useRouter();
 
   useEffect(() => {
     if (data && !isLoading) {
@@ -33,30 +35,37 @@ const Navbar = () => {
           </Link>
         </>
       </div>
-      <div className="flex-none gap-[40px] mx-[20px]">
-        {address && data && (
-          <Popover.Root>
-            <Popover.Trigger asChild>
-              <div className="flex flex-row gap-[8px] items-center cursor-pointer">
-                <span className="font-bold text-[20px] text-[#9381FF]">
-                  {isLoading ? "Loading..." : data.value.toString()}
-                </span>
-                <img src="/icons/royale-coin.svg" className="w-[35px]" alt="" />
-              </div>
-            </Popover.Trigger>
-            <Popover.Portal>
-              <Popover.Content
-                className="rounded p-5 w-[260px] bg-white shadow-[0_10px_38px_-10px_hsla(206,22%,7%,.35),0_10px_20px_-15px_hsla(206,22%,7%,.2)] focus:shadow-[0_10px_38px_-10px_hsla(206,22%,7%,.35),0_10px_20px_-15px_hsla(206,22%,7%,.2),0_0_0_2px_theme(colors.violet7)] will-change-[transform,opacity] data-[state=open]:data-[side=top]:animate-slideDownAndFade data-[state=open]:data-[side=right]:animate-slideLeftAndFade data-[state=open]:data-[side=bottom]:animate-slideUpAndFade data-[state=open]:data-[side=left]:animate-slideRightAndFade outline-none focus:outline-none"
-                sideOffset={5}
-              >
-                <MintRC />
-                <Popover.Arrow className="fill-white" />
-              </Popover.Content>
-            </Popover.Portal>
-          </Popover.Root>
-        )}
-        <ConnectWallet theme="light" className="connect-button" />
-      </div>
+
+      {router.pathname !== "/landing" && (
+        <div className="flex-none gap-[40px] mx-[20px]">
+          {address && data && (
+            <Popover.Root>
+              <Popover.Trigger asChild>
+                <div className="flex flex-row gap-[8px] items-center cursor-pointer">
+                  <span className="font-bold text-[20px] text-[#9381FF]">
+                    {isLoading ? "Loading..." : data.value.toString()}
+                  </span>
+                  <img
+                    src="/icons/royale-coin.svg"
+                    className="w-[35px]"
+                    alt=""
+                  />
+                </div>
+              </Popover.Trigger>
+              <Popover.Portal>
+                <Popover.Content
+                  className="rounded p-5 w-[260px] bg-white shadow-[0_10px_38px_-10px_hsla(206,22%,7%,.35),0_10px_20px_-15px_hsla(206,22%,7%,.2)] focus:shadow-[0_10px_38px_-10px_hsla(206,22%,7%,.35),0_10px_20px_-15px_hsla(206,22%,7%,.2),0_0_0_2px_theme(colors.violet7)] will-change-[transform,opacity] data-[state=open]:data-[side=top]:animate-slideDownAndFade data-[state=open]:data-[side=right]:animate-slideLeftAndFade data-[state=open]:data-[side=bottom]:animate-slideUpAndFade data-[state=open]:data-[side=left]:animate-slideRightAndFade outline-none focus:outline-none"
+                  sideOffset={5}
+                >
+                  <MintRC />
+                  <Popover.Arrow className="fill-white" />
+                </Popover.Content>
+              </Popover.Portal>
+            </Popover.Root>
+          )}
+          <ConnectWallet theme="light" className="connect-button" />
+        </div>
+      )}
     </div>
   );
 };
