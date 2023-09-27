f3ebf046baca12ffdafc8a5804fc50562895a5d1
Ayush Jain <ayushjain2205@gmail.com>
2023-09-27 23:24:42 +0530

    Update view file

--- src/components/Buttons/ViewFile.js
+++ src/components/Buttons/ViewFile.js
@@ -1,20 +1,7 @@
 import React, { useState, useEffect } from "react";
 import dynamic from "next/dynamic";
 
-const FileViewer = dynamic(
-  () => import("react-file-viewer"),
-  { ssr: false } // This will load the component only on the client side.
-);
-
 const ViewFile = () => {
-  const [isClient, setIsClient] = useState(false); // New state variable
-
-  useEffect(() => {
-    setIsClient(true); // Set to true once the component has mounted
-  }, []);
-  const file =
-    "https://w7.pngwing.com/pngs/895/199/png-transparent-spider-man-heroes-download-with-transparent-background-free-thumbnail.png";
-  const type = "png";
   return (
     <div className="tooltip" data-tip="View File">
       <button
@@ -26,13 +13,6 @@ const ViewFile = () => {
       <dialog id="my_modal_2" className="modal">
         <div className="modal-box">
           <h3 className="font-bold text-lg">File</h3>
-          {isClient && (
-            <FileViewer
-              fileType={type}
-              filePath={file}
-              onError={(error) => console.log(error)}
-            />
-          )}
         </div>
         <form method="dialog" className="modal-backdrop">
           <button>close</button>
